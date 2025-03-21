8e37f8de3783e7b4f1fc45bb4524aeeaf5e10fab
MattLV21 <mattias.lange00@gmail.com>
2025-03-21 21:32:31 +0100

    Basic bevy setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use bevy::prelude::*;
+
+const WINDOW_WIDTH: f32 = 640.0; // Width of the window
+const WINDOW_HEIGHT: f32 = 480.0; // Height of the window
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+    .add_plugins(
+        DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Rust boids".into(),
+                    resolution: (WINDOW_WIDTH, WINDOW_HEIGHT).into(),
+                    ..default()
+                }),
+                ..default()
+            })
+            .build(),
+    )
+    .add_systems(Startup, setup)
+    .add_systems(Update, update)
+    .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+}
+
+fn update(_time: Res<Time>,) {
+
 }
