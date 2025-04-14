aa641764ad6270bed36b83c3906708202b64391d
MattLV21 <mattias.lange00@gmail.com>
2025-04-14 17:44:22 +0200

    Complete game of life

--- src/game_of_life.rs
+++ src/game_of_life.rs
@@ -1,17 +1,17 @@
 use std::collections::HashMap;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct LivingCell {
+pub struct Cell {
     pub x: i32,
     pub y: i32,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct ConwaysGame {
-    cells: Vec<LivingCell>,
+    cells: Vec<Cell>,
 }
 
-impl LivingCell {
+impl Cell {
     pub fn new(x: i32, y: i32) -> Self {
         Self {
             x,
@@ -21,29 +21,49 @@ impl LivingCell {
 }
 
 impl ConwaysGame {
-    pub fn new(starting: Vec<LivingCell>) -> Self {
+    pub fn new(starting: Vec<Cell>) -> Self {
         Self {
             cells: starting,
         }
     }
 
-    pub fn get_living_cells(&self) -> Vec<LivingCell> {
+    pub fn get_living_cells(&self) -> Vec<Cell> {
         return self.cells.clone();
     }
 
-    pub fn calculate_next_gen_map(&self) -> HashMap<LivingCell, u8> {
-        let mut potential_cells: HashMap<LivingCell, u8> = HashMap::new();
+    pub fn calculate_next_gen_map(&self) -> HashMap<Cell, u8> {
+        let mut potential_cells: HashMap<Cell, u8> = HashMap::new();
         for cell in self.cells.iter() {
             for x in -1..2 {
                 for y in -1..2 {
                     if x == 0 && y == 0 {
                         continue;
                     }
-                    let key = LivingCell::new(cell.x + x, cell.y + y);
+                    let key = Cell::new(cell.x + x, cell.y + y);
                     *potential_cells.entry(key).or_insert(0) += 1;
                 }
             }
         }
         return potential_cells;
+    }
+
+    pub fn apply_gen_from_map(&mut self, map: &HashMap<Cell, u8>) {
+        let mut new_gen: Vec<Cell> = Vec::new();
+        
+        for (cell, &count) in map.iter() {
+            if self.cells.contains(cell) {
+                // Cell is alive
+                if count == 2 || count == 3 {
+                    new_gen.push(cell.clone()); // survives
+                }
+            } else {
+                // Cell is dead
+                if count == 3 {
+                    new_gen.push(cell.clone()); // becomes alive
+                }
+            }
+        }
+    
+        self.cells = new_gen;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use bevy::prelude::*;
-use std::collections::HashMap;
 mod game_of_life;
-use game_of_life::{LivingCell, ConwaysGame};
+use game_of_life::{Cell, ConwaysGame};
 
 #[derive(Resource)]
 pub struct ConwaysGameResource(pub ConwaysGame);
+#[derive(Resource)]
+pub struct Last(f32);
 
 const WINDOW_WIDTH: f32 = 640.0; // Width of the window
 const WINDOW_HEIGHT: f32 = 480.0; // Height of the window
@@ -33,13 +34,14 @@ fn main() {
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2d);
 
-    let mut cells: Vec<LivingCell> = Vec::new();
-    for x in -5..5 {
-        for y in -5..5 {
-            cells.push(LivingCell::new(x, y));
+    let mut cells: Vec<Cell> = Vec::new();
+    for x in -20..20 {
+        for y in -20..20 {
+            cells.push(Cell::new(x, y));
         }
     }
     let game = ConwaysGameResource(ConwaysGame::new(cells));
+    let last = Last(0.0);
     
     for cell in game.0.get_living_cells().iter() {
         commands.spawn( (
@@ -51,7 +53,7 @@ fn setup(mut commands: Commands) {
             Transform::from_xyz((cell.x * (CELL_SIZE + CELL_MARGIN) as i32) as f32, (cell.y * (CELL_SIZE + CELL_MARGIN) as i32) as f32, 0.0), // Position
         ));
     }
-
+    commands.insert_resource(last);
     commands.insert_resource(game);
 }
 
@@ -64,11 +66,25 @@ fn clear_sprites(mut commands: Commands, query: Query<Entity, With<Sprite>>) {
     }
 }
 
-fn update(mut commands: Commands, _conways_game: Res<ConwaysGameResource>, _time: Res<Time>,) {
+fn update(mut commands: Commands, mut conways_game: ResMut<ConwaysGameResource>, time: Res<Time>, mut last_time: ResMut<Last>, keyboard_input: Res<ButtonInput<KeyCode>>) {
     // TODO
     // calcualte the next genaration
     // update conways game based to new genaration
-    for cell in _conways_game.0.get_living_cells().iter() {
+    let l_time = last_time.0;
+    let game = &mut conways_game.0;
+    if keyboard_input.just_pressed(KeyCode::Space) {
+    }
+    if time.elapsed_secs() > l_time + 1.0 {
+        let map = game.calculate_next_gen_map();
+        game.apply_gen_from_map(&map);
+        
+        last_time.0 = time.elapsed_secs();
+    }
+
+    // commands.resour;
+    // conways_game.0 = next_gen;
+
+    for cell in game.get_living_cells().iter() {
         commands.spawn( (
             Sprite {
                 color: Color::WHITE,
